45bbac9b03785d034787840cf1d5426463c64743
opixxx <opix0306@naver.com>
2024-05-13 23:24:21 +0900

    백준 11508 2+1 세일

--- src/greedy/_11508.java
+++ src/greedy/_11508.java
@@ -3,10 +3,7 @@ package greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 /*
